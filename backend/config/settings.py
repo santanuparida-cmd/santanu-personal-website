8fee90d490e1aff00a6744a5af45ff707e232735
Santanu Parida <santanu@Santanus-MacBook-Air.local>
2026-08-22 01:03:15 +0530

    Fix static file storage configuration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -140,7 +140,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
 
