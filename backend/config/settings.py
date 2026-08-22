ccfc2ff85aa74cf3f7fcbbecb7f595b37b6be670
Santanu Parida <santanu@Santanus-MacBook-Air.local>
2026-08-22 16:29:21 +0530

    Restrict CORS to production frontend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -147,7 +147,9 @@ STORAGES = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://santanuparida.netlify.app",
+]
 MEDIA_URL = "/media/"
 
 CLOUDINARY_STORAGE = {
